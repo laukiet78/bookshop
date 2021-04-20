f11e562f2f0cc35809ce7a6ea5fb67bfe211639d
Kiet Lau <laukiet78@yahoo.com>
2021-04-20 13:55:58 +0700

    Initialize Project

---
+++ main/urls.py
@@ -0,0 +1,38 @@
+from django.urls import path
+from django.views.generic import TemplateView
+from main import views
+from django.views.generic.detail import DetailView
+from main import models
+from django.contrib.auth import views as auth_views
+from main import forms
+
+urlpatterns = [
+    path(
+        "login/",
+        auth_views.LoginView.as_view(
+            template_name="login.html",
+            form_class=forms.AuthenticationForm,
+        ),
+        name="login",
+    ),
+    path('signup/', views.SignupView.as_view(), name="signup"),
+    path(
+        "product/<slug:slug>/",
+        DetailView.as_view(model=models.Product),
+        name="product",
+    ),
+    path(
+        "products/<slug:tag>/",
+        views.ProductListView.as_view(),
+        name="products",
+    ),
+    path("contact-us/",
+         views.ContactUsView.as_view(),
+         name="contact_us",),
+    path(
+        "about-us/",
+        TemplateView.as_view(template_name="about_us.html")),
+    path(
+        "",
+        TemplateView.as_view(template_name="home.html")),
+]
